5374da68fc372daca9bf5b143ca031f24a7bb8c3
bax082024 <bax082024@gmail.com>
2024-12-28 17:20:31 +0100

    added game variables

--- NewTetrisForm.cs
+++ NewTetrisForm.cs
@@ -2,6 +2,17 @@ namespace NewTetris
 {
     public partial class NewTetrisForm : Form
     {
+        private int gridWidth = 10; // Number of columns
+        private int gridHeight = 20; // Number of rows
+        private int cellSize = 30; // Size of each block in pixels
+        private Color[,] grid; // The grid to store colors of placed blocks
+        private Tetromino currentTetromino; // The current piece
+        private int score = 0; // Player's score
+        private int level = 1; // Current game level
+        private int fallSpeed = 500; // Speed in milliseconds
+        private System.Windows.Forms.Timer gameTimer = new System.Windows.Forms.Timer();
+        private Random random = new Random();
+
         public NewTetrisForm()
         {
             InitializeComponent();
