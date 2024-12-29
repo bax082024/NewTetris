efca6a8c3cc4421318652330edc4528315a28ec2
bax082024 <bax082024@gmail.com>
2024-12-29 01:50:40 +0100

    added highscoreform.cs

--- obj/Debug/net8.0-windows/NewTetris.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/NewTetris.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NewTetris")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fefd8cf9bdbe4dc10e9158776cb1107f2c10d253")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1aa765c9e3c93b79351dbcd595d9da9daf1af48")]
 [assembly: System.Reflection.AssemblyProductAttribute("NewTetris")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NewTetris")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
