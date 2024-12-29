a0f99eb550a2bd0e24f8bc1d3920ebcf81b5b32d
bax082024 <bax082024@gmail.com>
2024-12-29 10:58:22 +0100

    changed image

--- obj/Debug/net8.0-windows/NewTetris.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/NewTetris.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NewTetris")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc3492a4fb2f20361d7702892a81478d634081bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10ebca2fae696253ccecff3f3a06672a1368a176")]
 [assembly: System.Reflection.AssemblyProductAttribute("NewTetris")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NewTetris")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
