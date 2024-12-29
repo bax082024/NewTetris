2eee4563f7389974df2fc59b4ac046a24435f5a0
bax082024 <bax082024@gmail.com>
2024-12-29 01:53:03 +0100

    added datagridview

--- HighScoreForm.Designer.cs
+++ HighScoreForm.Designer.cs
@@ -28,18 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
+            panel1 = new Panel();
+            dataGridView1 = new DataGridView();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Transparent;
+            panel1.Controls.Add(dataGridView1);
+            panel1.Dock = DockStyle.Fill;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(339, 521);
+            panel1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(49, 73);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(240, 288);
+            dataGridView1.TabIndex = 0;
             // 
             // HighScoreForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(519, 580);
+            BackColor = SystemColors.Info;
+            ClientSize = new Size(339, 521);
+            Controls.Add(panel1);
             Name = "HighScoreForm";
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "HighScoreForm";
+            panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel panel1;
+        private DataGridView dataGridView1;
     }
 }
